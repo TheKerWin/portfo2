11cbc50b332728cae117dc28bc18c90fcdf786f3
Kerwin <kerwin.mercado@uconn.edu>
2024-01-24 23:19:59 -0500

    added images to individual pages

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,5 +1,6 @@
 import { getProject } from "@/sanity/sanity-utils";
 import { PortableText } from "@portabletext/react";
+import Image from "next/image";
 
 
 type Props = {
@@ -33,12 +34,17 @@ export default async function Project({ params }: Props) {
       </header>
       {/* content goes header */}
       <div className="text-lg text-gray-700 mt-5">
-        <div>
           <PortableText value={project.content} />
-        </div>
       </div>
 
       {/* image goes here */}
+      <Image
+      src ={project.image}
+      alt={project.name}
+      width={1920}
+      height={1080}
+      className="mt-10 border-2 border-gray-700 object-cover rounded-xl"
+      />
     </div>
   );
 }
