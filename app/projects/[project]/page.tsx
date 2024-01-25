44401abe84034b254e7886c8b6c9fd30e4aab60c
Kerwin <kerwin.mercado@uconn.edu>
2024-01-24 23:14:13 -0500

    stylizing individual pages

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,13 +1,44 @@
 import { getProject } from "@/sanity/sanity-utils";
+import { PortableText } from "@portabletext/react";
+
 
 type Props = {
-    params: {project: string}
+  params: { project: string };
 };
 
 export default async function Project({ params }: Props) {
-    const slug = params.project; 
-    const project = await getProject(slug);
+  const slug = params.project;
+  const project = await getProject(slug);
 
-    return <div>{project.name} </div>
+  return (
+    <div className="max-w-3xl mx-auto py-20">
+      {/* Header goes here */}
+      <header className="flex items-center justify-between">
+        <h1
+          className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text
+      text-transparent text-5xl drop-shadow font-extrabold p-1"
+        >
+          {project.name}
+        </h1>
 
+        <a
+          href={project.url}
+          title="View Project"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-pink-500 hover:text-pink-100 transition"
+        >
+          View Project
+        </a>
+      </header>
+      {/* content goes header */}
+      <div className="text-lg text-gray-700 mt-5">
+        <div>
+          <PortableText value={project.content} />
+        </div>
+      </div>
+
+      {/* image goes here */}
+    </div>
+  );
 }
